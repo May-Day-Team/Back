ab0b0076705f518f7564e342b8c398e196ac9a0f
sonjihoon <141906696+sonj1hoon@users.noreply.github.com>
2024-12-05 17:12:51 +0900

    241205 추가수정

--- calendar/calendar/src/main/java/org/aba2/calendar/common/domain/group/db/GroupRepository.java
+++ calendar/calendar/src/main/java/org/aba2/calendar/common/domain/group/db/GroupRepository.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package org.aba2.calendar.common.domain.group.db;
 
 import org.aba2.calendar.common.domain.group.model.GroupEntity;
@@ -9,14 +9,3 @@ import java.util.Optional;
 public interface GroupRepository extends JpaRepository<GroupEntity, String> {
 
 }
-=======
-package org.aba2.calendar.common.domain.group.db;
-
-import org.aba2.calendar.common.domain.group.model.GroupEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<GroupEntity, String> {
-
-
-}
->>>>>>> diary
