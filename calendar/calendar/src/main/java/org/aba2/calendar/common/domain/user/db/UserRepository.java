45420ce7c8355b4f05223af780ed44cac713a575
바카로이 <haeun8520@naver.com>
2024-12-06 15:03:10 +0900

    캘린더 충돌
    캘린더 충돌

--- calendar/calendar/src/main/java/org/aba2/calendar/common/domain/user/db/UserRepository.java
+++ calendar/calendar/src/main/java/org/aba2/calendar/common/domain/user/db/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 
-    Optional<UserEntity> findByIdOrPhoneNumber(String id, String phoneNumber);
+    Optional<UserEntity> findByUserIdOrPhoneNumber(String id, String phoneNumber);
 
 }
